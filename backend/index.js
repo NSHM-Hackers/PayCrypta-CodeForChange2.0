327958e43c31323c75c6264693a6397bc0cd45ab
Sumana Mondal <sumanamondal329@gmail.com>
2026-04-10 12:40:11 +0530

    add db.js for database connection and create .env example for understand

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,18 @@ dotenv.config();
 
 import cors from "cors";
 import apiRoutes from "./routes/index.js";
+import { connectDB } from "./utils/db.js";
 
+// The __dirname variable does not work in ES modules, so we need to create it manually
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Create an expressjs server
 const app = express();
+// Connect to the database
+connectDB();
 
-
+// Add middleware to parse JSON and handle CORS
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
